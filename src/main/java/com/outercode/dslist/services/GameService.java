4fa9dbea63462fea83666c20a552ed3f49a67b44
Vladimir Monteiro Souza de Lima <vladimir.monteiro021@gmail.com>
2024-11-13 20:37:09 -0300

    find game by id

--- src/main/java/com/outercode/dslist/services/GameService.java
+++ src/main/java/com/outercode/dslist/services/GameService.java
@@ -1,9 +1,12 @@
 package com.outercode.dslist.services;
 
+import com.outercode.dslist.dto.GameDTO;
 import com.outercode.dslist.dto.GameMinDTO;
+import com.outercode.dslist.entities.Game;
 import com.outercode.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,8 +16,20 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        Game result = gameRepository.findById(id).get();
+        return new GameDTO(result);
+
+    }
+
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll(){
         var result = gameRepository.findAll();
         return result.stream().map(GameMinDTO::new).toList();
     }
+
+
 }
